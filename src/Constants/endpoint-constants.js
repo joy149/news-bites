c82cceab80193afe4fee3d3769de4e037788c8e3
joy149 <jbhwmck149@gmail.com>
2022-12-27 15:58:39 +0530

    Fetching news items through api

---
+++ src/Constants/endpoint-constants.js
@@ -0,0 +1,10 @@
+const Endpoint_Constants = {
+    'topHeadlines' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188"
+    },
+    'altImage' : {
+        url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1OoOi19ZAhGRYQDlRVgAhTc3XSHFqkgND5Q&usqp=CAU"
+    }
+}
+
+export default Endpoint_Constants;
