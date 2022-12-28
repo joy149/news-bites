3b267bfef9c894b2557eb0979ad3ea6b0d67c048
joy149 <jbhwmck149@gmail.com>
2022-12-29 00:57:45 +0530

    Defining categories and routing as per category logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,104 @@
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom'
+import About from './Components/About';
+import Endpoint_Constants  from './Constants/endpoint-constants'
 
 export default class App extends Component {
+  landingPage = [
+    {
+      url: Endpoint_Constants.topHeadlines.url,
+      title: "Top Headlines",
+      component: News,
+      path: "/",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.sports.url,
+      title: "Sports",
+      component: News,
+      path: "/sports",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.business.url,
+      title: "Business",
+      path: "/business",
+      component: News,
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.entertainment.url,
+      title: "Entertainment",
+      component: News,
+      path: "/entertainment",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.science.url,
+      title: "Science",
+      component: News,
+      path: "/science",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.tech.url,
+      title: "Technology",
+      component: News,
+      path: "/technology",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.general.url,
+      title: "General",
+      component: News,
+      path: "/general",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: Endpoint_Constants.health.url,
+      title: "Health",
+      component: News,
+      path: "/health",
+      pageSize: 6,
+      exact: true
+    },
+    {
+      url: "",
+      title: "About Us",
+      component: About,
+      path: "/about",
+      pageSize: 0,
+      exact: true
+    }
+  ]
   render() {
     return (
-      <div>
-        <Navbar />
-        <News pageSize={3} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            {this.landingPage.map((item) => {
+              return (
+                <Route key={item.url} exact={item.exact} path={item.path} element={<item.component key={item.path.slice(1)} title={item.title} pageSize={item.pageSize} url={item.url} />} >
+                </Route>
+              )
+            })}
+          </Routes>
+        </div>
+      </BrowserRouter>
     )
   }
 }
--- src/Constants/endpoint-constants.js
+++ src/Constants/endpoint-constants.js
@@ -4,6 +4,27 @@ const Endpoint_Constants = {
     },
     'altImage' : {
         url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1OoOi19ZAhGRYQDlRVgAhTc3XSHFqkgND5Q&usqp=CAU"
+    },
+    'sports' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=sports&page="
+    },
+    'business' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=business&page="
+    },
+    'entertainment' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=entertainment&page="
+    },
+    'science' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=science&page="
+    },
+    'tech' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=technology&page="
+    },
+    'general' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=general&page="
+    },
+    'health' : {
+        url : "https://newsapi.org/v2/top-headlines?country=in&sortBy=publishedAt&apiKey=983c934239e94e50acef1bdf98234188&category=health&page="
     }
 }
 
